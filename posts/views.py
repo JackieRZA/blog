75ba86d2262a5dd4516948d98ce055f0c9236f84
JackieRZA <jediminds29@gmail.com>
2022-07-22 14:41:31 +0300

    add homework

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,44 @@
 
+import logging
+
 from django.http import HttpResponse
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
-    if request.GET.get("key") == "test":
-        return HttpResponse("Posts withggg test key")
-    return HttpResponse("Posts index view")
+    test_var_1 = settings.TEST_VAR_1
+    logger.info(f"\n\tTEST_VAR_1 = {test_var_1}")
+
+    # Print environment variables to console
+    print_test_var_2_and_3 = settings.PRINT_TEST_VAR_2_AND_3
+    test_var_2 = settings.TEST_VAR_2
+    test_var_3 = settings.TEST_VAR_3
+
+    if print_test_var_2_and_3 == "True":
+        text = "\n\tPRINT_TEST_VAR_2_AND_3 = {0}\n\tTEST_VAR_2 = {1}\n\tTEST_VAR_3 = {2}\n"
+        env_var = text.format(print_test_var_2_and_3, test_var_2, test_var_3)
+    else:
+        env_var = "\n\tPRINT_TEST_VAR_2_AND_3 = {0}\n".format(print_test_var_2_and_3)
+
+    logger.info(env_var)
+
+    return HttpResponse(f"<h1>Posts index view.</h1>")
+
+
+def post(request):
+    if request.POST:
+        data = request.POST.dict()
+        for key, value in data.items():
+            logger.info(f"POST param: {key} = {value}")
+
+        return HttpResponse("<h1>It's POST method for 'posts'</h1>")
+
+    data = request.GET.dict()
+
+    text_for_response = f"<h1>It's GET method for 'posts'</h1><p>Data:</p>"
+    rows = [f"\n<br>{key} = {value}" for key, value in data.items()]
+    text_for_response = text_for_response + "".join(rows)
+
+    return HttpResponse(text_for_response)
